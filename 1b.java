ec250e77ce09525ad6b1652a519b21297d3de1be
Armaanjijotia <armaanjijotia009@gmail.com>
2025-03-26 00:17:03 +0530

    Add files via upload

---
+++ 1b.java
@@ -0,0 +1,11 @@
+class Main {
+    int id;
+    String name;
+
+    public static void main(String[] args) {
+        Main s1 = new Main();  // Create object of Main class
+        s1.id = 1010;
+        s1.name = "jijotia";
+        System.out.println(s1.id + " " + s1.name);
+    }
+}
